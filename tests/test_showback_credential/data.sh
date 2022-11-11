14637e17a0f961dca657e78f28e7239703cf3350
alexis-de-carvalho <alexis.martins-de-carvalho@epita.fr>
2022-11-11 11:03:51 +0100

    testsuite: showback credential added

---
+++ tests/test_showback_credential/data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source config.sh
+
+RESOURCE="showback-credential"
+RESOURCE_NAME=test-"$RESOURCE"
+DEST=$PRE$RESOURCE$END
+WAITING=180
+
+export CP_SHOWBACK_CRED_NAME="$RESOURCE_NAME"-"$RANDOM"
+echo -n "$CP_SHOWBACK_CRED_NAME" > ref
+export CP_SHOWBACK_CRED_URL="https://prometheus.taikun.dev/"
+export CP_SHOWBACK_CRED_USER=$PROMETHEUS_USER
+
+echo -n "$PROMETHEUS_PASSWORD" | base64 > passwordb
+export CP_SHOWBACK_CRED_PASSWORD_B=`cat passwordb`
+rm passwordb
+
+export CP_SHOWBACK_CRED_ORG="test-all-organization-attach"
+export CP_SHOWBACK_CRED_LOCK="false"
