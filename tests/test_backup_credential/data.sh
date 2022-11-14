1da712040754ac55b7dedf0f78e8980a74e61f2b
alexis-de-carvalho <alexis.martins-de-carvalho@epita.fr>
2022-11-14 14:58:56 +0100

    testsuite: backup credentials

---
+++ tests/test_backup_credential/data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source config.sh
+
+RESOURCE="backup-credential"
+RESOURCE_NAME=test-"$RESOURCE"
+DEST=$PRE$RESOURCE$END
+WAITING=180
+
+export CP_BACKUP_CRED_NAME="$RESOURCE_NAME"-"$RANDOM"
+echo -n "$CP_BACKUP_CRED_NAME" > ref
+export CP_BACKUP_CRED_ORG="test-all-organization-attach"
+export CP_BACKUP_CRED_ACCESS_KEY=$S3_ACCESS_KEY
+export CP_BACKUP_CRED_ENDPOINT="https://test-backup-region.taikun.dev"
+export CP_BACKUP_CRED_REGION="taikun-east-1"
+export CP_BACKUP_CRED_LOCK="false"
+
+echo -n "$S3_SECRET_KEY" | base64 > passwordb
+export CP_BACKUP_CRED_SECRET_KEY_B=`cat passwordb`
+rm passwordb
