a82b6c3ccacb3b4e0ffd89d34b422df7c9d8b771
alexis-de-carvalho <alexis.martins-de-carvalho@epita.fr>
2022-12-20 15:52:44 +0100

    testsuite-updated: reput other tests

---
+++ tests/test_slack_configuration/data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source config.sh
+
+RESOURCE="slack-configuration"
+RESOURCE_NAME=test-"$RESOURCE"
+DEST=$PRE$RESOURCE$END
+WAITING=180
+
+export CP_SC_NAME="slack-$TAIKUN_USER-$RANDOM"
+echo -n "$CP_SC_NAME" > ref
+export CP_SC_ORG="test-$TAIKUN_USER-org-attach"
+export CP_SC_CHANNEL="random"
+export CP_SC_TYPE="General"
+export CP_SC_URL="https://test-crossplane.slack.com/archives/C043Z7TNESZ"
