d75409656fa9cba0d74ee5c204a67f47ffc57cdf
alexis-de-carvalho <alexis.martins-de-carvalho@epita.fr>
2023-01-04 18:34:48 +0100

    testsuite-updated: removed deletions

--- tests/run_requirements.sh
+++ tests/run_requirements.sh
@@ -94,5 +94,6 @@ source get_cloud_ref.sh
 
 #kubectl apply -f required-test-project.yaml
 #source get_project_ref.sh
+#sleep 600
 
 #sleep 35
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -22,14 +22,14 @@ while read curr; do
     then
         cd ..
         rm .out*
-        kubectl delete -f required-test-billing-rule.yaml
-        kubectl delete -f required-test-billing-credential.yaml
-        kubectl delete -f required-test-showback-credential.yaml
-        kubectl delete -f required-test-cloud-credential.yaml
+        #kubectl delete -f required-test-billing-rule.yaml
+        #kubectl delete -f required-test-billing-credential.yaml
+        #kubectl delete -f required-test-showback-credential.yaml
+        #kubectl delete -f required-test-cloud-credential.yaml
         kubectl delete -f secret_cloud.yaml
-        kubectl delete -f required-test-kubernetes-profile.yaml
-        kubectl delete -f required-test-user.yaml
-        kubectl delete -f required-test-organization.yaml
+        #kubectl delete -f required-test-kubernetes-profile.yaml
+        #kubectl delete -f required-test-user.yaml
+        #kubectl delete -f required-test-organization.yaml
         #kubectl delete -f required-test-project.yaml
         rm get*
         rm required*
@@ -44,16 +44,16 @@ done < ".out_test"
 
 rm .out*
 
-kubectl delete -f required-test-billing-rule.yaml
-kubectl delete -f required-test-billing-credential.yaml
-kubectl delete -f required-test-showback-credential.yaml
+#kubectl delete -f required-test-billing-rule.yaml
+#kubectl delete -f required-test-billing-credential.yaml
+#kubectl delete -f required-test-showback-credential.yaml
 
-kubectl delete -f required-test-cloud-credential.yaml
-kubectl delete -f secret_cloud.yaml
+#kubectl delete -f required-test-cloud-credential.yaml
+#kubectl delete -f secret_cloud.yaml
 
-kubectl delete -f required-test-kubernetes-profile.yaml
-kubectl delete -f required-test-user.yaml
-kubectl delete -f required-test-organization.yaml
+#kubectl delete -f required-test-kubernetes-profile.yaml
+#kubectl delete -f required-test-user.yaml
+#kubectl delete -f required-test-organization.yaml
 
 #kubectl delete -f required-test-project.yaml
 
